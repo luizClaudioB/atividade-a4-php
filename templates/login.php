4221ce1a4c31036db411f37d533ee98781ebb898
luizClaudioB <47514047+luizClaudioB@users.noreply.github.com>
2020-05-25 21:24:23 -0300

    Update login.php

--- templates/login.php
+++ templates/login.php
@@ -6,7 +6,7 @@ echo '
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="default.css">
+        <link rel="stylesheet" href="styles.css">
         <script src="index.js"></script>
         <title>Index</title>
     </head>
